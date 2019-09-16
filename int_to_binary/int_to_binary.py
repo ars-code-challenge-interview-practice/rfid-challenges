be7c79e657959735fa7692343355fee93088e935
arsummers <aliyasummers1@gmail.com>
2019-09-16 11:06:25 -0700

    setup done

---
+++ int_to_binary/int_to_binary.py
@@ -0,0 +1,5 @@
+"""
+1.     Write a method that takes in an integer and returns its binary representation in string form. So, if the input is 3 the output is '11', similarly if the input is 128, the output is '10000000'. Don't use the builtin 'bin' function for your solution.
+
+Now enhance the output by adding a space to separate all the nibbles. This is how a calculator typically shows binary output. 128 would now look like this '1000 0000', and a number like 60 would look like this '11 1100'
+"""
